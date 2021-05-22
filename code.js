cb37f14211ed27bf802c4415ad67b88a2d8de025
Victoria Rossiyskaya <meowvoto@gmail.com>
2021-05-22 18:56:31 +0300

    source of images is ghanged

--- code.js
+++ code.js
@@ -1,12 +1,26 @@
-let images = document.querySelectorAll(".image");
-// console.log(images);
-let sliderImages = [];
-for (let i = 0; i < images.length; i++) {
-    let newSliderImage = {
-        image: images[i],
-        title: i+1
-    }
-    sliderImages.push(newSliderImage)
-}
+// let images = document.querySelectorAll(".image");
+// // console.log(images);
+// let sliderImages = [];
+// for (let i = 0; i < images.length; i++) {
+//     let newSliderImage = {
+//         image: images[i],
+//         title: i+1
+//     }
+//     sliderImages.push(newSliderImage)
+// }
 
-console.log(sliderImages);
+// console.log(sliderImages);
+// sliderImages[0].image.style.cssText = "display: block";
+
+let images = [
+    {url:'https://www.anicoremixgallery.com/wp-content/uploads/2021/03/8_EGO-GRIN_new.jpg',
+    title:'EGO-GRIN, YONEYAMA MAI'}, 
+    {url:'https://www.anicoremixgallery.com/wp-content/uploads/2021/03/006_EGO-SCAR.jpg',
+    title: 'EGO-SCAR, YONEYAMA MAI'}, 
+    {url:'https://www.anicoremixgallery.com/wp-content/uploads/2021/03/007_EGO-BEAT.jpg',
+    title: 'EGO-BIT, YONEYAMA MAI'}, 
+    {url:'https://www.anicoremixgallery.com/wp-content/uploads/2021/03/003_EGO-DNA.jpg',
+    title: 'EGO-DNA, YONEYAMA MAI'}, 
+    {url:'https://www.anicoremixgallery.com/wp-content/uploads/2021/03/004_EGO-MELT.jpg',
+    title:'EGO-MELT, YONEYAMA MAI'}, 
+]
